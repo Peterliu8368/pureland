24b2b37b206679cc49a09e3094b67ad05a525162
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-09-21 11:02:18 -0700

    add basic signup route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,7 +6,11 @@ router.get('/', (req, res)=>{
 })
 
 router.post('/signup', (req, res)=>{
-    console.log(req.body.name);
+    const {name, email, password} = req.body
+    if (!email || !password || !name){
+        return res.status(422).json({error:"please add all the fields"})
+    }
+    res.json({message:"Successfully posted"})
 })
 
 module.exports = router;
