46eb110623424d6c33872e8ad0a113b63ea63242
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-09-21 13:42:33 -0700

    add jwt

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGOURL: "mongodb+srv://peter:YecLsKlzlyEpSGP9@cluster0.gclh4.mongodb.net/userdb?retryWrites=true&w=majority"
+    MONGOURL: "mongodb+srv://peter:YecLsKlzlyEpSGP9@cluster0.gclh4.mongodb.net/userdb?retryWrites=true&w=majority",
+    JWT_SECRET: "apounmkkucsarfiji12312jkdsai3245489,./.sad;;fsd!#@!#$&*@!$32498"
 }
