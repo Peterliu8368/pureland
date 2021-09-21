cebfd4efbdbed89ce2d62ad50923dbf18d84532a
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-09-21 15:05:43 -0700

    add  create, allpost, mypost route

---
+++ server/routes/post.routes.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const requireLogin = require('../middleware/requireLogin');
+const Post = mongoose.model("Post");
+
+router.get('/allpost', requireLogin, (req, res)=>{
+    Post.find()
+    .populate('postedBy', "_id name")
+        .then(posts=>{
+            res.json({posts})
+        })
+        .catch(err=> console.log(err))
+})
+
+router.post('/createpost', requireLogin, (req, res)=>{
+    const {title, body, pho} = req.body
+    if (!title || !body){
+        return res.status(422).json({error:"Please add all the required fields"})
+    }
+    req.user.password = undefined
+    Post.create({
+        ...req.body,
+        postedBy: req.user
+    })
+        .then(result => {
+            res.json({post: result})
+        })
+        .catch(err=>console.log(err))
+})
+
+router.get('/mypost', requireLogin, (req, res)=>{
+    Post.find({postedBy: req.user._id})
+        .populate('postedBy', "_id name")
+        .then(myPosts => {
+            res.json({myPosts})
+        })
+        .catch(err => console.log(err))
+})
+
+module.exports = router
