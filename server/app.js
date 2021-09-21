ae1a3e3c54da8347136848c01c51d7ea4bfdd055
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-09-21 11:02:12 -0700

    add body parser

--- server/app.js
+++ server/app.js
@@ -7,12 +7,16 @@ const {MONGOURL} = require('./keys.js')
 //get mongodb model
 require('./models/user.model');
 
+//body parser
+app.use(express.json())
+
+
 //use router
 app.use(require('./routes/auth'));
 
+
 //connect to mongo db
 mongoose.connect(MONGOURL);
-
 mongoose.connection.on('connected', ()=> {
     console.log('connected to mongo db hurray');
 })
