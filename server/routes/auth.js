878da357d2a07745cc3dd07a4c89003ac356d63c
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-09-21 11:27:52 -0700

    hash password before store into db

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose')
 const user = mongoose.model("User")
+const bcrypt = require('bcryptjs')
 
-router.get('/', (req, res)=>{
-    res.send("Hello")
-})
 
 router.post('/signup', (req, res)=>{
     const {name, email, password} = req.body
@@ -17,9 +15,12 @@ router.post('/signup', (req, res)=>{
             if(savedUser.length > 0){
                 return res.status(422).json({error:"user already exists with that email"})
             }
-            user.create(req.body)
-                .then(user => res.json({message: "saved successfully"}))
-                .catch(err=>console.log(err))
+            bcrypt.hash(password, 12)
+                .then(hashedPassword=>{
+                    user.create({...req.body, password: hashedPassword})
+                    .then(user => res.json({message: "saved successfully"}))
+                    .catch(err=>console.log(err))
+                })
         }).
         catch(err=>console.log(err))
 })
