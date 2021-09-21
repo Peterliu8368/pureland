2ec526061c0e10dc2a7c08cb676fddd0ff33b1a9
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-09-21 09:03:27 -0700

    initialize server

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+const mongoose = require("mongoose");
+const {MONGOURL} = require('./keys.js')
+
+//get mongodb model
+require('./models/user.model');
+
+//use router
+app.use(require('./routes/auth'));
+
+//connect to mongo db
+mongoose.connect(MONGOURL);
+
+mongoose.connection.on('connected', ()=> {
+    console.log('connected to mongo db hurray');
+})
+mongoose.connection.on('error', (err)=> {
+    console.log('error connecting', err);
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+})
