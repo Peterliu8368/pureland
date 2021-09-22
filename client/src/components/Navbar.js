508db7548c12e339963464cbb958538630d8b536
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-09-22 08:35:27 -0700

    add Link

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar=()=>{
     return (
         <nav>
-            <div className="nav-wrapper">
-            <a href="#" className="brand-logo">Logo</a>
-            <ul id="nav-mobile" className="right">
-                <li><a href="sass.html">Sass</a></li>
-                <li><a href="badges.html">Components</a></li>
-                <li><a href="collapsible.html">JavaScript</a></li>
-            </ul>
+            <div className="nav-wrapper white">
+                <Link to="/" className="brand-logo left">Pureland</Link>
+                <ul id="nav-mobile" className="right">
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/signup">Signup</Link></li>
+                    <li><Link to="/profile">Profile</Link></li>
+                </ul>
             </div>
         </nav>
     )
