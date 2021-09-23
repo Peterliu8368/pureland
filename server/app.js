9eabe02c7dc494699b186933113cfd475398db88
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-09-23 16:38:06 -0700

    add code for production

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 const { urlencoded } = require("express");
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
-const {MONGOURL} = require('./keys.js')
+const {MONGOURL} = require('./config/keys.js')
 
 //connect to mongo db
 mongoose.connect(MONGOURL);
@@ -25,6 +25,14 @@ app.use(require('./routes/auth.routes'));
 app.use(require('./routes/post.routes'));
 app.use(require('./routes/user.routes'));
 
+//production code
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
