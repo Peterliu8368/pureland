70c96ff8802faaa60f789d8c37c811e08df7d523
peter-codes <39788956+Peterliu8368@users.noreply.github.com>
2021-09-24 15:41:52 -0700

    Delete dev.js

--- server/config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    MONGOURL: "mongodb+srv://peter:PGw9Hdx39LndO3qg@cluster0.gclh4.mongodb.net/userdb?retryWrites=true&w=majority",
-    JWT_SECRET: "apounmkkuc78123dasdi12312jkdsai3245489,./.sad;;fsd!#@!#$&*@!$32498",
-    SENDGRID_API: "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",
-    EMAIL: "http://localhost:3000"
-}
